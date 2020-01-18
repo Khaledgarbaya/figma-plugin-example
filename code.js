696038089be82eb5c57e82edab975bb69b15db7c
Khaled Garbaya <khaledgarbaya@gmail.com>
2020-01-18 19:10:45 +0100

    Initial commit

---
+++ code.js
@@ -0,0 +1,8 @@
+figma.showUI(__html__);
+const selection = figma.currentPage.selection['0'];
+figma.ui.postMessage({ selection: selection.characters });
+figma.ui.onmessage = event => {
+    if (event.type === 'close') {
+        figma.closePlugin();
+    }
+};
