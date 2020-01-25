be5d951b4d3dccf1724e9949ebb29564722dcf9f
Khaled Garbaya <khaledgarbaya@gmail.com>
2020-01-25 20:33:05 +0100

    set node text

--- code.js
+++ code.js
@@ -1,8 +1,31 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 figma.showUI(__html__);
-const selection = figma.currentPage.selection['0'];
-figma.ui.postMessage({ selection: selection.characters });
-figma.ui.onmessage = event => {
+figma.ui.onmessage = (event) => __awaiter(this, void 0, void 0, function* () {
+    if (event.type === 'request:characters') {
+        const selection = figma.currentPage.selection['0'];
+        figma.ui.postMessage({
+            selection: (selection && selection.characters) || '',
+            type: 'response:characters'
+        });
+        return;
+    }
+    if (event.type === 'request:setText') {
+        const selection = figma.currentPage.selection['0'];
+        if (selection) {
+            yield figma.loadFontAsync(selection.fontName);
+            selection.characters = event.text;
+        }
+        return;
+    }
     if (event.type === 'close') {
         figma.closePlugin();
     }
-};
+});
--- code.ts
+++ code.ts
@@ -1,10 +1,23 @@
 figma.showUI(__html__)
 
-const selection = figma.currentPage.selection['0']
+figma.ui.onmessage = async event => {
+  if (event.type === 'request:characters') {
+    const selection = figma.currentPage.selection['0']
+    figma.ui.postMessage({
+      selection: (selection && selection.characters) || '',
+      type: 'response:characters'
+    })
+    return
+  }
+  if (event.type === 'request:setText') {
+    const selection = figma.currentPage.selection['0']
+    if (selection) {
+      await figma.loadFontAsync(selection.fontName)
+      selection.characters = event.text
+    }
+    return
+  }
 
-figma.ui.postMessage({ selection: selection.characters })
-
-figma.ui.onmessage = event => {
   if (event.type === 'close') {
     figma.closePlugin()
   }
